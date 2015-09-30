a3df8c80894b05478b05233b969162876d98c40b
Bobsum <rasmus@bobsum.dk>
2015-09-30 17:44:12 +0200

    Created ScoreClient

--- Plan2015.Score.ConsoleClient/ScoreClient.cs
+++ Plan2015.Score.ConsoleClient/ScoreClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNet.SignalR.Client;
 using Plan2015.Dtos;
 
@@ -7,25 +8,15 @@ namespace Plan2015.Score.ConsoleClient
 {
     class ScoreClient
     {
+        private readonly IDictionary<int, SchoolScore> _schoolScores = new Dictionary<int, SchoolScore>();
+        
         public ScoreClient()
         {
             var connection = new HubConnection("http://localhost:2015/");
 
             var hub = connection.CreateHubProxy("ScoreHub");
 
-            hub.On<IEnumerable<SchoolScoreDto>>("Updated", schools =>
-            {
-                _schools = schools;
-                Console.Clear();
-                foreach (var school in _schools)
-                {
-                    Console.WriteLine(school.Name);
-                    foreach (var house in school.Houses)
-                    {
-                        Console.WriteLine("  {0} : {1}", house.Name, house.Amount);
-                    }
-                }
-            });
+            hub.On<IEnumerable<SchoolScoreDto>>("Updated", UpdateSchoolScores);
 
             connection.Start().ContinueWith(task =>
             {
@@ -33,13 +24,82 @@ namespace Plan2015.Score.ConsoleClient
                 {
                     Console.WriteLine("There was an error opening the connection:{0}", task.Exception.GetBaseException());
                 }
-                else
-                {
-                    Console.WriteLine("Connected");
-                }
             }).Wait();
         }
 
-        private IEnumerable<SchoolScoreDto> _schools;
+        private void UpdateSchoolScores(IEnumerable<SchoolScoreDto> schools)
+        {
+            foreach (var school in schools)
+            {
+                SchoolScore score;
+                if (!_schoolScores.TryGetValue(school.Id, out score))
+                {
+                    score = new SchoolScore
+                    {
+                        Id = school.Id, Name = school.Name
+                    };
+                    _schoolScores.Add(school.Id, score);
+                }
+                score.UpdateHouseScores(school.Houses);
+            }
+
+            Console.Clear();
+            foreach (var schoolScore in SchoolScores)
+            {
+                Console.WriteLine("{0} : {1}", schoolScore.Name, schoolScore.Amount);
+                foreach (var houseScore in schoolScore.HouseScores)
+                {
+                    Console.WriteLine("{0} : {1}", houseScore.Name, houseScore.Amount);
+                }
+            }
+        }
+
+        public IEnumerable<SchoolScore> SchoolScores
+        {
+            get { return _schoolScores.Values; }
+        }
+    }
+
+    public class SchoolScore
+    {
+        private readonly IDictionary<int, HouseScore> _houseScores = new Dictionary<int, HouseScore>();
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IEnumerable<HouseScore> HouseScores
+        {
+            get { return _houseScores.Values; }
+        }
+
+        public int Amount
+        {
+            get { return HouseScores.Sum(h => h.Amount); }
+        }
+
+        public void UpdateHouseScores(IEnumerable<HouseScoreDto> houses)
+        {
+            foreach (var house in houses)
+            {
+                HouseScore score;
+                if (!_houseScores.TryGetValue(house.Id, out score))
+                {
+                    score = new HouseScore
+                    {
+                        Id = house.Id,
+                        Name = house.Name
+                    };
+                    _houseScores.Add(house.Id, score);
+                }
+                score.Amount = house.Amount;
+            }
+        }
+    }
+
+    public class HouseScore
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; }
     }
 }
