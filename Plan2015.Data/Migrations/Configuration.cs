16be737ae30a48b40f1ffdfca1378a0aff4699c5
Bobsum <rasmus@bobsum.dk>
2015-10-05 22:22:20 +0200

    Fixed score and added new seed

--- Plan2015.Data/Migrations/Configuration.cs
+++ Plan2015.Data/Migrations/Configuration.cs
@@ -15,38 +15,48 @@ namespace Plan2015.Data.Migrations
             var schools = new[]
             {
                 new School {Name = "Agernholdt"},
-                new School {Name = "Hardenberg"},
-                new School {Name = "Ravnsborg"}
+                new School {Name = "Ravnsborg"},
+                new School {Name = "Hardenberg"}
             };
             context.Schools.AddOrUpdate(s => s.Name, schools);
             var houses = new[]
             {
-                new House {Name = "A1", School = schools[0]},
-                new House {Name = "A2", School = schools[0]},
-                new House {Name = "A3", School = schools[0]},
-                new House {Name = "A4", School = schools[0]},
-                new House {Name = "H1", School = schools[1]},
-                new House {Name = "H2", School = schools[1]},
-                new House {Name = "H3", School = schools[1]},
-                new House {Name = "H4", School = schools[1]},
-                new House {Name = "R1", School = schools[2]},
-                new House {Name = "R2", School = schools[2]},
-                new House {Name = "R3", School = schools[2]},
-                new House {Name = "R4", School = schools[2]}
+                new House {Name = "Guldberg", School = schools[0]},
+                new House {Name = "Basse", School = schools[0]},
+                new House {Name = "Adamsen", School = schools[0]},
+                new House {Name = "Lauridsen", School = schools[0]},
+                new House {Name = "Jensen", School = schools[1]},
+                new House {Name = "Witt", School = schools[1]},
+                new House {Name = "Blumensaat", School = schools[1]},
+                new House {Name = "Glarbo", School = schools[1]},
+                new House {Name = "Kolze", School = schools[2]},
+                new House {Name = "Fjord", School = schools[2]},
+                new House {Name = "Bruun", School = schools[2]},
+                new House {Name = "Malling", School = schools[2]}
             };
             context.Houses.AddOrUpdate(h => h.Name, houses);
 
-            context.Scouts.AddOrUpdate(s => s.Name,
-                new Scout { Name = "Spejder 1", Rfid = "0003375441", House = houses[0]},
-                new Scout { Name = "Spejder 2", Rfid = "0003375442", House = houses[0]},
-                new Scout { Name = "Spejder 3", Rfid = "0003375443", House = houses[0]},
-                new Scout { Name = "Spejder 4", Rfid = "0003375444", House = houses[0]},
-                new Scout { Name = "Spejder 5", Rfid = "0003375445", House = houses[0]},
-                new Scout { Name = "Spejder 6", Rfid = "0003375446", House = houses[0]},
-                new Scout { Name = "Spejder 7", Rfid = "0003375447", House = houses[0]},
-                new Scout { Name = "Spejder 8", Rfid = "0003375448", House = houses[1]},
-                new Scout { Name = "Spejder 9", Rfid = "0003375449", House = houses[1]}
-                );
+            for (int i = 0; i < 84; i++)
+            {
+               context.Scouts.AddOrUpdate(s => s.Name, new Scout
+                {
+                    Name = "Spejder " + (i + 1),
+                    Rfid = (i + 1).ToString("D10"),
+                    House = houses[i/7]
+                });
+            }
+
+            /*context.Scouts.AddOrUpdate(s => s.Name,
+                new Scout { Name = "Spejder 1", Rfid = "0000000001", House = houses[0]},
+                new Scout { Name = "Spejder 2", Rfid = "0000000002", House = houses[0]},
+                new Scout { Name = "Spejder 3", Rfid = "0000000003", House = houses[0]},
+                new Scout { Name = "Spejder 4", Rfid = "0000000004", House = houses[4]},
+                new Scout { Name = "Spejder 5", Rfid = "0000000005", House = houses[4]},
+                new Scout { Name = "Spejder 6", Rfid = "0000000006", House = houses[4]},
+                new Scout { Name = "Spejder 7", Rfid = "0000000007", House = houses[8]},
+                new Scout { Name = "Spejder 8", Rfid = "0000000008", House = houses[8]},
+                new Scout { Name = "Spejder 9", Rfid = "0000000009", House = houses[8]}
+                );*/
 
             context.TeamMembers.AddOrUpdate(t => t.Name,
                 new TeamMember { Name = "TeamMember 1", Rfid = "0003375431" },
--- Plan2015.Web/Repository.cs
+++ Plan2015.Web/Repository.cs
@@ -32,27 +32,20 @@ namespace Plan2015.Web
                     p.Amount
                 });
 
-            return ap
-                .Concat(pp)
-                .Concat(tp)
-                .GroupBy(p => p.House, p => p.Amount, (h, a) => new
-                {
-                    h.School,
-                    House = h,
-                    Amount = a.Sum()
-                })
-                .ToList()
-                .GroupBy(p => p.School, p => new HouseScoreDto
-                {
-                    Id = p.House.Id,
-                    Name = p.House.Name,
-                    Amount = p.Amount
-                }, (s, h) => new SchoolScoreDto
+            return db.Schools
+                .Select(s => new SchoolScoreDto
                 {
                     Id = s.Id,
                     Name = s.Name,
-                    Houses = h
-                }).ToList();
+                    Houses = s.Houses
+                        .Select(h => new HouseScoreDto
+                        {
+                            Id = h.Id,
+                            Name = h.Name,
+                            Amount = ap.Concat(pp).Concat(tp).Where(p => p.House == h).Sum(p => (int?) p.Amount) ?? 0
+                        })
+                })
+                .ToList();
         }
 
         public PunctualityStatusDto GetPunctualityStatus(DataContext db, int id)
@@ -73,7 +66,7 @@ namespace Plan2015.Web
                         {
                             Id = h.Id,
                             Name = h.Name,
-                            Scouts = h.Scouts.Select(s => new PunctualityStatusScoutDto
+                            Scouts = h.Scouts.Where(s => !s.Home).Select(s => new PunctualityStatusScoutDto
                             {
                                 Id = s.Id,
                                 Name = s.Name,
